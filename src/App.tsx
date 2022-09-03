e712e56b9de4922f9852d09776347c16ac8e086f
ShenyiCui <shenyicui@outlook.com>
2022-09-04 05:37:41 +0800

    feat: init Prettier and Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-black">Hello world!</h1>
-  );
+  return <h1 className='text-3xl font-bold underline text-black'>Hello world!</h1>;
 }
 
 export default App;
