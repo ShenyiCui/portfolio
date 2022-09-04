5a67a27184181de05976dc810a45ba7199a2e985
ShenyiCui <shenyicui@outlook.com>
2022-09-05 01:40:12 +0800

    feat: add mobile responsiveness to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <div className='h-64 mt-8 flex justify-center'>
       <div className='h-full w-full max-w-7xl rounded-t-md bg-lightBlue flex flex-col items-center justify-center'>
-        <h1 className='text-2xl font-montserrat font-bold'>Thanks for checking out my work!</h1>
-        <h1 className='text-2xl font-montserrat font-bold text-darkBlue'>Let&apos;s Connect!</h1>
+        <h1 className='text-xl md:text-2xl font-montserrat font-bold'>Thanks for checking out my work!</h1>
+        <h1 className='text-xl md:text-2xl font-montserrat font-bold text-darkBlue'>Let&apos;s Connect!</h1>
         <div className='flex space-x-5 my-2.5'>
           <Icon iconType={IconType.LinkedIn} vSpace='mt-0' link='https://www.linkedin.com/in/shenyi-cui-4b9b95211/' />
           <Icon iconType={IconType.Github} vSpace='mt-0' link='https://github.com/ShenyiCui' />
