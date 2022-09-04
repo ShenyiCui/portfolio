b0208710e53414b405226ce42f8c6214d8b66aae
ShenyiCui <shenyicui@outlook.com>
2022-09-04 18:05:50 +0800

    feat: prepare skeleton project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import { PaperClipIcon } from '@heroicons/react/20/solid';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid';
+
+import ionic from '@/assets/icons/ionic.svg';
+import github from '@/assets/icons/github.svg';
+import figma from '@/assets/icons/figma.svg';
+import open from '@/assets/icons/open-in-new.svg';
 
 const ProjectCard = () => {
   return (
     <div className='overflow-hidden shadow-lg bg-white border rounded-lg my-5'>
       <div className='px-4 py-5 sm:px-6'>
-        <h3 className='text-lg font-medium leading-6 text-gray-900 font-montserrat'>Project Name</h3>
+        <h3 className='text-2xl font-bold leading-6 text-gray-900 font-montserrat'>Project Name</h3>
         <p className='mt-1 max-w-2xl text-sm text-gray-500 font-inter'>MAY 2022 - JUN 2022</p>
       </div>
       <div className='border-t border-gray-200 px-4 py-5 sm:px-6 font-inter'>
@@ -21,32 +26,60 @@ const ProjectCard = () => {
           </div>
           <div className='sm:col-span-1'>
             <dt className='text-sm font-medium text-gray-500'>Tech Stack</dt>
-            <dd className='mt-1 text-sm text-gray-900'>Margot Foster</dd>
+            <dd className='mt-1 flex space-x-3'>
+              <div className='w-8 grid grid-cols-1 h-5 group'>
+                <div className='flex justify-center'>
+                  <img src={ionic} style={{ width: 30 }} className='m-0' />
+                </div>
+                <div className='hidden justify-center -mt-2 group-hover:flex'>
+                  <p className='text-[10px] text-black m-0'>Ionic</p>
+                </div>
+              </div>
+            </dd>
+          </div>
+
+          <div className='sm:col-span-1'>
+            <dt className='text-sm font-medium text-gray-500'>Links</dt>
+            <dd className='mt-1 flex space-x-3'>
+              <div className='w-8 grid grid-cols-1 h-5 group cursor-pointer'>
+                <div className='flex justify-center'>
+                  <img src={github} style={{ width: 30 }} className='m-0' />
+                </div>
+                <div className='hidden justify-center -mt-2 group-hover:flex'>
+                  <p className='text-[10px] text-black m-0'>Github</p>
+                </div>
+              </div>
+              <div className='w-8 grid grid-cols-1 h-5 group cursor-pointer'>
+                <div className='flex justify-center'>
+                  <img src={figma} style={{ width: 30 }} className='m-0' />
+                </div>
+                <div className='hidden justify-center -mt-2 group-hover:flex'>
+                  <p className='text-[10px] text-black m-0'>Design/Figma</p>
+                </div>
+              </div>
+              <div className='w-8 grid grid-cols-1 h-5 group cursor-pointer'>
+                <div className='flex justify-center'>
+                  <img src={open} style={{ width: 30 }} className='m-0' />
+                </div>
+                <div className='hidden justify-center -mt-2 group-hover:flex'>
+                  <p className='text-[10px] text-black m-0'>Visit</p>
+                </div>
+              </div>
+            </dd>
           </div>
 
           <div className='sm:col-span-2'>
-            <dt className='text-sm font-medium text-gray-500'>Links</dt>
+            <dt className='text-sm font-medium text-gray-500'>Other</dt>
             <dd className='mt-1 text-sm text-gray-900'>
               <ul role='list' className='divide-y divide-gray-200 rounded-md border border-gray-200'>
                 <li className='flex items-center justify-between py-3 pl-3 pr-4 text-sm'>
                   <div className='flex w-0 flex-1 items-center'>
-                    <PaperClipIcon className='h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
-                    <span className='ml-2 w-0 flex-1 truncate'>resume_back_end_developer.pdf</span>
+                    <ArrowTopRightOnSquareIcon className='h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
+                    <span className='ml-2 w-0 flex-1 truncate'>Devpost</span>
                   </div>
                   <div className='ml-4 flex-shrink-0'>
-                    <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className='flex items-center justify-between py-3 pl-3 pr-4 text-sm'>
-                  <div className='flex w-0 flex-1 items-center'>
-                    <PaperClipIcon className='h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
-                    <span className='ml-2 w-0 flex-1 truncate'>coverletter_back_end_developer.pdf</span>
-                  </div>
-                  <div className='ml-4 flex-shrink-0'>
-                    <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                      Download
+                    <a href='#' className='font-medium text-darkBlue hover:text-mediumBlue'>
+                      Visit
                     </a>
                   </div>
                 </li>
