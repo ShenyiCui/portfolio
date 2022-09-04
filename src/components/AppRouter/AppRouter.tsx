2748bb64aa069b5d78634fac4ec657014fee052e
ShenyiCui <shenyicui@outlook.com>
2022-09-04 11:58:27 +0800

    feat: add not found 404 routes

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -7,6 +7,10 @@ const AppRouter = () => {
     <Switch>
       <Route exact path={Routes.home} />
       <Route exact path={Routes.aboutMe} />
+      <Route exact path={Routes.notFound} />
+      <Route exact path='*'>
+        <Redirect to={Routes.notFound} />
+      </Route>
     </Switch>
   );
 };
