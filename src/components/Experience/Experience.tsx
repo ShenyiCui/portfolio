1c1a8a3300a60ee73fb255f6b922197f4f2c9472
ShenyiCui <shenyicui@outlook.com>
2022-09-06 18:46:23 +0800

    fix: remove timeline on mobile

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -28,10 +28,17 @@ const Experience = ({ event, eventIdx, eventLength }: Prop) => {
   return (
     <li>
       <div className='relative pb-8'>
-        {eventIdx !== eventLength ? <span className='absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200' aria-hidden='true' /> : null}
+        {eventIdx !== eventLength ? (
+          <span className='absolute top-4 hidden left-4 -ml-px h-full w-0.5 bg-gray-200 md:block' aria-hidden='true' />
+        ) : null}
         <div className='relative flex space-x-3'>
           <div>
-            <span className={classNames(event.iconBackground, 'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white')}>
+            <span
+              className={classNames(
+                event.iconBackground,
+                'h-8 w-8 rounded-full hidden md:flex items-center justify-center ring-8 ring-white',
+              )}
+            >
               <event.icon className='h-5 w-5 text-white' aria-hidden='true' />
             </span>
           </div>
